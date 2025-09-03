191af687a9e01546022520359ec117cd7c7c4820
Moamen Eltouny <raggigroup@gmail.com>
2025-09-03 23:47:44 +0300

    Add Article model and Sluggable tests; remove unused content field from migration

---
+++ tests/SluggableTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Pharaonic\Laravel\Sluggable\Tests;
+
+use Pharaonic\Laravel\Sluggable\Tests\Models\Article;
+
+class SluggableTest extends TestCase
+{
+    public function test_it_can_generate_a_slug()
+    {
+        $article = Article::create([
+            'title'   => 'Test Title',
+        ]);
+
+        $this->assertSame('test-title', $article->slug);
+        $this->assertSame('1-test-title', $article->slugWithKey);
+    }
+
+    public function test_it_can_generate_slug_in_ascii_only()
+    {
+        config(['pharaonic.sluggable.ascii_only' => true]);
+
+        $article = Article::create([
+            'title'   => 'تجربة عنوان',
+        ]);
+
+        $this->assertSame('tgrb-aanoan', $article->slug);
+    }
+
+    public function test_it_can_find_by_slug()
+    {
+        Article::create([
+            'title'   => 'Test Title',
+        ]);
+
+        $this->assertModelExists(Article::findBySlug('test-title'));
+        $this->assertTrue(Article::whereSlug('test-title')->exists());
+    }
+
+    public function test_it_can_fail_to_find_by_slug()
+    {
+        $this->assertNull(Article::findBySlug('non-existent-slug'));
+    }
+}
--- tests/database/migrations/create_articles_table.php
+++ tests/database/migrations/create_articles_table.php
@@ -11,7 +11,6 @@ return new class () extends Migration {
             $table->id();
             $table->string('title');
             $table->sluggable();
-            $table->text('content')->nullable();
             $table->timestamps();
         });
     }
